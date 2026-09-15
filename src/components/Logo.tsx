cd0ad3246da0e25c9a52b9487d82bdc604df5cdd
Jolly Christopher <jollychris131@gmail.com>
2026-09-15 12:14:42 +0100

    fix: use repository SVG logo in React

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,4 @@
-import logo from '../assets/bi-quicker-logo.png'
+import logo from '../assets/bi-quicker-logo.svg'
 
 export function Logo({ small = false }: { small?: boolean }) {
   return <img className={`logo-mark ${small ? 'logo-small' : ''}`} src={logo} alt="Bi-quicker" />
