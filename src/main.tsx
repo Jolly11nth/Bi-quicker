a2e5fbb7979d7871cd6943710cdb120dc9ae8001
Jolly Christopher <jollychris131@gmail.com>
2026-09-15 17:37:19 +0100

    Load commerce flow styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import '../styles.css'
 import './logo-overrides.css'
+import './commerce.css'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
