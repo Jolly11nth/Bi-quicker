44b65c9043f83df999cac7074cb38be009bce66d
Jolly Christopher <jollychris131@gmail.com>
2026-09-15 18:39:59 +0100

    Remove unused back fallback import

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
 import { Dashboard } from './pages/Dashboard'
 import { AdminChatOverview, OrderCenter } from './pages/OrderCenterV2'
-import { roleFromSegment, signInPath } from './lib/roles'
+import { roleFromSegment } from './lib/roles'
 import type { RoleKey } from './lib/types'
 
 type CommerceMode = 'shop' | 'list' | 'order'
