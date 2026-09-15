0790bf9c6bd70624f9bfb5994b622e574ce2f113
Jolly Christopher <jollychris131@gmail.com>
2026-09-15 17:57:56 +0100

    Enable automatic rider assignment in the app shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { AutoRiderAssignment } from './components/AutoRiderAssignment'
 import '../styles.css'
 import './logo-overrides.css'
 import './commerce.css'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <AutoRiderAssignment />
     <App />
   </React.StrictMode>,
 )
