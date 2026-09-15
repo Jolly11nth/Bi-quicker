7c8788235e2e15834d5e80160d111dc505f6f64c
Jolly Christopher <jollychris131@gmail.com>
2026-09-15 14:01:52 +0100

    fix: display supplied logo without cropping

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,14 @@
 import logo from '../assets/bi-quicker-logo.png'
+import './logo-fix.css'
 
 export function Logo({ small = false }: { small?: boolean }) {
-  return <img className={`logo-mark ${small ? 'logo-small' : ''}`} src={logo} alt="Bi-quicker" />
+  return (
+    <img
+      className={`logo-mark ${small ? 'logo-small' : ''}`}
+      src={logo}
+      alt="Bi-quicker"
+      draggable={false}
+      style={{ width: small ? 145 : 170, height: 'auto', objectFit: 'contain' }}
+    />
+  )
 }
