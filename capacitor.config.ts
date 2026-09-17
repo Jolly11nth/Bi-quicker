fc0dc415d73223ec51b0a66b7be0468a8a0660ba
Jolly Christopher <jollychris131@gmail.com>
2026-09-17 16:59:13 +0100

    Configure Capacitor for Bi-quicker

---
+++ capacitor.config.ts
@@ -0,0 +1,15 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.biquicker.app',
+  appName: 'Bi-quicker',
+  webDir: 'dist',
+  android: {
+    backgroundColor: '#ffffff',
+  },
+  ios: {
+    backgroundColor: '#ffffff',
+  },
+}
+
+export default config
